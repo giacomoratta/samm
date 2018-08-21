4328636a7f5f587ab121d4fe0345575d2bafbb6c
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-08-21 14:36:32 +0200

    mocha tests 0.1

---
+++ test/libs/DataManager.T1.test.js
@@ -0,0 +1,9 @@
+const test_config = require('../require.js');
+
+describe('DataManager.class', function() {
+    describe('#setRelationship()', function() {
+        it("set a relation between file and object", function() {
+
+        });
+    });
+});
