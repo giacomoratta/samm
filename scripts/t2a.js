76a16644f21a25c42ace9b10793a0d1cbe359f0a
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-08-21 09:47:17 +0200

    mocha tests 0.1

--- scripts/t2a.js
+++ scripts/t2a.js
@@ -5,7 +5,7 @@ global.DT = require('../libs/DirectoryTree.js');
 Utils.File.walkDirectory("L:\\MPL\\One Shots\\Cymatics - Vocals",{
     nodeCallback:(item)=>{
         // callback for each item
-if(item.isDirectory===true)console.log(item);
+        if(item.isDirectory===true) console.log(item);
         // if(item.isFile===true) console.log(item);
         // if(item.isDirectory===true) console.log(item);
     },
---
+++ test/test.js
@@ -0,0 +1,8 @@
+let assert = require('assert');
+describe('libs', () => {
+    require('./libs/testA.js');
+});
+
+describe('modules', () => {
+    require('./modules/testB.js');
+});
