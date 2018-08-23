307611b7effd015bfc00604556d615dffbb2e93a
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-08-23 16:26:35 +0200

    data settings

--- data_setting.js
+++ data_setting.js
@@ -2,6 +2,7 @@ DataMgr.setHolder({
     label:'scan_index',
     filePath:ConfigMgr.path('samples_index'),
     fileType:'json',
+    preLoad:true,
     checkFn:(dataObj,args)=>{
         return (dataObj && !dataObj.error());
     },
@@ -10,17 +11,24 @@ DataMgr.setHolder({
     },
     setFn:($cfg,args)=>{
         let tt = new DirectoryTree(ConfigMgr.path('samples_directory'));
-        tt.read();
-        if(!tt.error()) {
-            return tt;
+        try{
+            tt.read();
+            if(!tt.error()) return tt;
+        }catch(e){
+            return null;
         }
         return null;
     },
     loadFn:(fileData,$cfg,args)=>{
         if(!_.isObject(fileData)) return null;
         let tt = new DirectoryTree(ConfigMgr.path('samples_directory'));
-        tt.fromJson(fileData);
-        if(!tt.error()) return tt;
+        try{
+            tt.fromJson(fileData);
+            if(!tt.error()) return tt;
+        }catch(e){
+            return null;
+        }
+        return null;
     },
     saveFn:(dataObj,$cfg,args)=>{
         if(!$cfg.checkFn(dataObj)) return;
