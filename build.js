706fb86c669f2409132dc9e9aa75a4280f795bc4
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-11-06 16:42:25 +0100

    build

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-const nexe_compiler = require('nexe');
+const { compile } = require('nexe');
 const package_json = require('./package.json');
 global._ =  require('lodash');
 global.Utils = require('./libs/Utils.js');
@@ -58,19 +58,60 @@ Utils.File.removeDirSync(b$cfg.build_dir);
 console.log("\n"+'Create directory for build:',b$cfg.build_dir);
 Utils.File.ensureDirSync(b$cfg.build_dir);
 
-Utils.EXIT();
+let compileForPlatform = function(platform){
+    if(!b$cfg.platforms[platform]) return false;
+    let poz = b$cfg.platforms[platform];
+
+    poz.signature = platform.replace(/[\_]/g,'.').replace(/[^a-zA-Z0-9\.]/g,'');
+    poz.signature = 'mpl.'+b$cfg.version+'.'+poz.signature;
+
+    poz.compile_options = _.merge(b$cfg.compile_options,poz.compile_options);
+
+    poz.build_dir = Utils.File.pathJoin(b$cfg.build_dir,poz.signature);
+    poz.compile_options.output = Utils.File.pathJoin(poz.build_dir,'mpl');
+    poz.config_sample_copy = Utils.File.pathJoin(poz.build_dir,'config.sample.json');
+
+    console.log("\n"+'['+platform+'] Create directory for build:',poz.build_dir);
+    Utils.File.ensureDirSync(poz.build_dir);
+
+    console.log("\n"+'['+platform+'] Copying sample config file:',poz.config_sample_copy);
+    Utils.File.copyFileSync(poz.config_sample, poz.config_sample_copy);
+
+    console.log(poz.compile_options);
+
+    return compile(poz.compile_options).then(() => {
+        console.log("\n"+'['+platform+'] Build complete:',poz.compile_options.output,"\n");
+    }).catch(() => {
+        console.log("\n"+'['+platform+'] Build failed:',poz.compile_options.output,"\n");
+    });
+}
+
+let platformsToBuildFor = [
+    'mac_x64',
+    //'win_x64',
+    //'win_x86',
+    'linux_x64',
+    //'linux_x86'
+];
+
+if(platformsToBuildFor.length>0){
+    let _recursivePromise = function(a,i){
+        return compileForPlatform(a[i]).then(()=>{
+            if(i>=a.length){
+                console.log('FINISHED');
+            }else{
+                return _recursivePromise(a,i+1);
+            }
+        }).catch((e)=>{
+            console.error('ERROR',e);
+        });
+    }
+    _recursivePromise(platformsToBuildFor,0);
+}
+
+//Utils.EXIT();
 
 /*
-* create directory ./build/mpl.0.0.0.mac.x64
-* create file      ./build/mpl.0.0.0.mac.x64/config.sample.json from config.sample.unix.json
-* create directory ./build/mpl.0.0.0.win.x86
-* create file      ./build/mpl.0.0.0.win.x86/config.sample.json from config.sample.win.json
-* create directory ./build/mpl.0.0.0.win.x64
-* create file      ./build/mpl.0.0.0.win.x64/config.sample.json from config.sample.win.json
-* create directory ./build/mpl.0.0.0.linux.x86
-* create file      ./build/mpl.0.0.0.linux.x86/config.sample.json from config.sample.unix.json
-* create directory ./build/mpl.0.0.0.linux.x64
-* create file      ./build/mpl.0.0.0.linux.x64/config.sample.json from config.sample.unix.json
 * compile for mac.x64
 * compile for win.x86
 * compile for win.x64
@@ -78,11 +119,11 @@ Utils.EXIT();
 * compile for linux.x64
 * make zip files and delete original directories
 * */
-
-nexe_compiler({
-    input: './app-prod.js',
-    output: './build/y',
-    verbose:true
-}).then(() => {
-    console.log('success')
-});
+//
+// compile({
+//     input: './app-prod.js',
+//     output: './build/y',
+//     verbose:true
+// }).then(() => {
+//     console.log('success')
+// });
