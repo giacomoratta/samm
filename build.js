2f905ababd9948dc87557bfdef0f4b5d55bd1935
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-11-06 11:45:40 +0100

    build

--- build.js
+++ build.js
@@ -1,10 +1,26 @@
-const { compile } = require('nexe');
+const compile = require('nexe');
+const package_json = require('./package.json');
+const Utils = require('./libs/Utils.js');
 
 /*
-* read package version
-* create 3 directories
-* copy config.samples
-* make zip files
+* delete directory ./build with rimraf
+* create directory ./build
+* create directory ./build/mpl.0.0.0.mac.x64
+* create file      ./build/mpl.0.0.0.mac.x64/config.sample.json from config.sample.unix.json
+* create directory ./build/mpl.0.0.0.win.x86
+* create file      ./build/mpl.0.0.0.win.x86/config.sample.json from config.sample.win.json
+* create directory ./build/mpl.0.0.0.win.x64
+* create file      ./build/mpl.0.0.0.win.x64/config.sample.json from config.sample.win.json
+* create directory ./build/mpl.0.0.0.linux.x86
+* create file      ./build/mpl.0.0.0.linux.x86/config.sample.json from config.sample.unix.json
+* create directory ./build/mpl.0.0.0.linux.x64
+* create file      ./build/mpl.0.0.0.linux.x64/config.sample.json from config.sample.unix.json
+* compile for mac.x64
+* compile for win.x86
+* compile for win.x64
+* compile for linux.x86
+* compile for linux.x64
+* make zip files and delete original directories
 * */
 
 compile({
