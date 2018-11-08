3779407c54fcba61e7f69d478b85aa0fe3c990dd
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-11-08 10:25:06 +0100

    up

---
+++ www/layout.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Layout</title>
+</head>
+<body>
+
+<header>
+    <h1>MPL: a creativity-oriented samples manager</h1>
+    <div class="button"><a href="#">Download latest version</a></div>
+    <div><a href="#">Releases history</a></div>
+</header>
+
+
+<section>
+    <h2>Getting started</h2>
+
+    <h4>command option title</h4>
+    <section class="text">
+        <p>...abc...</p>
+        <div class="code_syntax">
+            mpl$ command opz 23
+        </div>
+        <p>...abc...</p>
+        <p>...abc...</p>
+    </section>
+</section>
+
+
+<section>
+    <h2>Documentation</h2>
+    <section class="text">
+        <p>...abc...</p>
+    </section>
+
+    <section class="command">
+        <h3>command title</h3>
+        <section class="text">
+            <p>...abc...</p>
+            <div class="code_syntax">
+                mpl$ command opz 23
+            </div>
+            <p>...abc...</p>
+            <h5>same font-size bold margin</h5>
+            <p>...abc...</p>
+
+            <section class="option">
+                <h4>option title</h4>
+                <section class="text">
+                    <p>...abc...</p>
+                    <div class="code_syntax">
+                        mpl$ command opz 23
+                    </div>
+                    <p>...abc...</p>
+                    <p>...abc...</p>
+                </section>
+            </section>
+
+        </section>
+    </section>
+</section>
+
+<footer>
+    <div>info info info</div>
+</footer>
+
+</body>
+</html>
