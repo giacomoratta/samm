3cec160fb6c1cf7a1b57bb978ac5c09205731dd3
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-11-09 14:34:17 +0100

    up

--- www/layout.php
+++ www/layout.php
@@ -144,55 +144,7 @@ $TemplateMgr = new TemplateManager();
             <p>menu with descriprions and anchors</p>
         </section>
 
-        <section class="command">
-            <h3>coverage</h3>
-            <section class="text">
-                <p class="code">
-                    mpl$ coverage
-                </p>
-                <p>
-                    Check how many samples are covered by the queries stored in the configuration or by an inline query;
-                    then prints the uncovered samples.
-                </p>
-                <p>
-                    By analysing the uncovered samples it is possible to add or modify configured queries in order
-                    to match more samples inside the collections. If many uncovered samples are left, the search results
-                    will be incomplete and some samples could be never used.
-                </p>
-                <p>
-                    <strong>Default behaviour.</strong> It works with Samples Directory and tagged queries (see Configuration).
-                    For each tagged query, this command prints how many samples are covered.
-                    At the end, it prints the list of uncovered samples.
-                </p>
-
-                <section class="option">
-                    <?php $TemplateMgr->printCommandOption('custom absolute path','-p, --path <path>'); ?>
-                    <p class="code">
-                        mpl$ coverage -p "/Users/default/Downloads/New Samples Big Pack"
-                    </p>
-                    <section class="text">
-                        <p>
-                            Let's suppose that you bought hundred of new samples. Before addind them to your samples collection
-                            you may want see if they are well covered by your tagged queries and which ones are uncovered.
-                            After that you can fix your configured tagged queries.
-                        </p>
-                    </section>
-                </section>
-
-                <section class="option">
-                    <?php $TemplateMgr->printCommandOption('custom query','-q, --query <query>'); ?>
-                    <p class="code">
-                        mpl$ coverage -q tag1+tag2,tag3
-                    </p>
-                    <section class="text">
-                        <p>
-                            Checks the coverage provided by the custom query instead of working with configured tagged queries.
-                        </p>
-                    </section>
-                </section>
-
-            </section>
-        </section>
+        <?php include('sections/command/coverage.php'); ?>
 
         <section class="command">
             <h3>command title</h3>
