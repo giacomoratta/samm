6f1a8b7b0092519938f8f221eb6f2860437f4e98
Giacomo Ratta <giacomo@hulpmiddelwereld.nl>
2018-11-23 10:57:08 +0100

    bookm

--- modules/ExportManager.js
+++ modules/ExportManager.js
@@ -8,26 +8,11 @@ class ExportManager {
 
 
     exportBookmarks(options){
-        let _self = this;
-        options = _.merge({
-            sourcePath:null,
-            destPath:null,
-            compressionLevel:1,
-            archiveNameFn:null
-        },options);
-        return new Promise(function(res,rej){
-            if(!Utils.File.directoryExistsSync(options.destPath)){
-                return rej({ code:'ENOENT_DEST' });
-            }
-
+        options.sourcePath=null;
+        return this.__export(function(archive, o /*parsed options*/){
             let archive_name = 'mpl_bookmarks';
-            let archive_path = Utils.File.checkAndSetDuplicatedFileNameSync( Utils.File.pathJoin( options.destPath, archive_name+'_'+Utils.dateToYYYYMMDD()+'.zip' ) );
+            let archive_path = Utils.File.checkAndSetDuplicatedFileNameSync( Utils.File.pathJoin( o.destPath, archive_name+'_'+Utils.dateToYYYYMMDD()+'.zip' ) );
 
-            // create a file to stream archive data to.
-            let output = Utils.File._FS.createWriteStream(archive_path);
-            let archive = _self._ARCHIVER('zip', {
-                zlib: { level: options.compressionLevel } // Sets the compression level.
-            });
             archive.append(null, { name: archive_name+'/' }); // ROOT
 
             BookmarksMgr.forEach(function(value,index,label,diffLb){
@@ -37,62 +22,50 @@ class ExportManager {
                 }
                 archive.append(value.path, { name: archive_name+'/'+label+'/'+value.base });
             });
-
-            output.on('close', function() {
-                res({
-                    total_bytes:archive.pointer()
-                });
-            });
-
-            //output.on('end', function() { console.log('Data has been drained'); });
-
-            archive.on('warning', function(err) {
-                rej(err);
-            });
-
-            archive.on('error', function(err) {
-                rej(err);
-            });
-            archive.pipe(output);
-            archive.finalize();
-        });
+            return {
+                archive_path:archive_path
+            };
+        },options);
     }
+
 
     exportProject(options){
-        options.archiveNameFn = function(o){
-            return Utils.File.checkAndSetDuplicatedFileNameSync( Utils.File.pathJoin( o.destPath, Utils.File.pathBasename(o.sourcePath)+'_'+Utils.dateToYYYYMMDD()+'.zip' ) );
-        };
-        return this.exportDirectory(options);
+        return this.__export(function(archive, o /*parsed options*/){
+            let archive_path = Utils.File.checkAndSetDuplicatedFileNameSync(
+                Utils.File.pathJoin( o.destPath, Utils.File.pathBasename(o.sourcePath)+'_'+Utils.dateToYYYYMMDD()+'.zip' )
+            );
+            archive.directory(o.sourcePath, Utils.File.pathBasename(o.sourcePath));
+            return {
+                archive_path:archive_path
+            };
+        },options);
     }
 
 
-    exportDirectory(options){
+    __export(configFn,options){
         let _self = this;
         options = _.merge({
             sourcePath:null,
             destPath:null,
-            compressionLevel:1,
-            archiveNameFn:null
+            compressionLevel:9
         },options);
         return new Promise(function(res,rej){
 
-            if(!Utils.File.directoryExistsSync(options.sourcePath)){
+            if(options.sourcePath && !Utils.File.directoryExistsSync(options.sourcePath)){
                 return rej({ code:'ENOENT_SOURCE' });
             }
-            if(!Utils.File.directoryExistsSync(options.destPath)){
+            if(options.destPath && !Utils.File.directoryExistsSync(options.destPath)){
                 return rej({ code:'ENOENT_DEST' });
             }
 
-            let archive_path = '';
-            if(!_.isFunction(options.archiveNameFn)) archive_path=Utils.File.pathJoin(options.destPath,Utils.File.pathBasename(options.sourcePath)+'.zip');
-            else archive_path=options.archiveNameFn(options);
+            let archive = _self._ARCHIVER('zip', {
+                zlib: { level: o.compressionLevel } // Sets the compression level.
+            });
+
+            let config = configFn(archive, options);
 
             // create a file to stream archive data to.
-            let output = Utils.File._FS.createWriteStream(archive_path);
-            let archive = _self._ARCHIVER('zip', {
-                zlib: { level: options.compressionLevel } // Sets the compression level.
-            });
-            archive.directory(options.sourcePath, Utils.File.pathBasename(options.sourcePath));
+            let output = Utils.File._FS.createWriteStream(config.archive_path);
 
             output.on('close', function() {
                 res({
