80c66b2c0a0060e8278a2ea12db0fb72ca5ab878
Giacomo <giacomo.ratta@diymaxeda.com>
2019-12-12 11:05:33 +0100

    refactoring 0.1

--- core/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-const appCore = require('../index');
-
-test('adds 1 + 2 to equal 3', () => {
-    console.log(appCore.config.set)
-    console.log(appCore.config.setfail)
-    expect(3).toBe(3);
-});
---
+++ core/config/__tests__/config.test.js
@@ -0,0 +1,11 @@
+const configField = require('../configField.class')
+
+describe('configField class and object', function() {
+
+    it("should create an empty configField", function() {
+        const configField1 = new configField()
+
+        //expect(configField.isValid()).toEqual(false)
+    })
+
+})
