306077d455c3f954a86f442dbd70c91a3c8a0728
Giacomo <giacomo.ratta@diymaxeda.com>
2019-12-13 08:52:19 +0100

    refactoring 0.1

--- core/cli-vorpal/__tests__/cliVorpal.test.js
+++ core/cli-vorpal/__tests__/cliVorpal.test.js
@@ -1,3 +1,58 @@
 const cliVorpal = new (require('../cliVorpal.class'))()
 
-cliVorpal.show('mpl')
+describe('cli interface with vorpal library', function() {
+
+    it("should add a command", function() {
+        expect(cliVorpal.addCommand('cmdname1 <action> [query]')).toEqual('cmdname1')
+        expect(cliVorpal.addCommand('cmdname2 ')).toEqual('cmdname2')
+
+        expect(cliVorpal.addCommandHeader('cmdname2')).toHaveProperty('description')
+        expect(cliVorpal.addCommandHeader('cmdname2')).toHaveProperty('option')
+
+        cliVorpal.addCommandHeader('cmdname2')
+            .description("Prints the samples collection" + '\n')
+            .option('-a, --all', 'Shows all the bookmarks')
+            .option('-l, --lookup', 'Shows the latest lookup')
+            .option('-t, --tag <tag>', 'Shows the bookmarks under the specified custom tag')
+            .option('-s, --save', 'Save bookmarks in the current project')
+    })
+
+
+    it("should set listener on events", function() {
+
+        expect(cliVorpal.on('show',()=>{
+            /* callback code */
+        })).toEqual(true)
+
+        expect(cliVorpal.on('exit',()=>{
+            /* callback code */
+        })).toEqual(true)
+
+        expect(cliVorpal.on('beforeNext',()=>{
+            /* callback code */
+        })).toEqual(true)
+
+        expect(cliVorpal.on('show-wrong',()=>{
+            /* callback code */
+        })).toEqual(false)
+    })
+
+
+    it("should set a custom logger", function() {
+
+        expect(cliVorpal.setLogger({
+            debug: ()=>{},
+            info: ()=>{},
+            warn: ()=>{},
+            error: ()=>{},
+            extra: ()=>{}
+        })).toEqual(true)
+
+        expect(cliVorpal.setLogger({
+            debug: ()=>{},
+            info: ()=>{},
+            warn: ()=>{},
+        })).toEqual(false)
+    })
+
+})
--- core/cli-vorpal/cliVorpal.class.js
+++ core/cli-vorpal/cliVorpal.class.js
@@ -1,9 +1,11 @@
+const events = require('events')
+const vorpal = require('vorpal')
 const cliParam = require('./cliParam.class.js')
 const cliPrinter = require('./cliPrinter.class.js')
-const vorpal = require('vorpal')
 
 const ERROR_CODE = -1
 const SUCCESS_CODE = 1
+const ACCEPTED_EVENTS = ['show','exit','beforeNext']
 
 class cliVorpal {
   constructor () {
@@ -11,15 +13,10 @@ class cliVorpal {
     this.vorpal = vorpal()
     this.delimiter = ''
     this.logger = console
-    this.callback = {
-      onShow: null,
-      onBeforeNext: null,
-      onExit: null
-    }
+    this.eventEmitter = new events()
 
     this.vorpal.on('client_prompt_submit', (command) => {
       if (command === 'exit') {
-        if(this.callback.onExit) this.callback.onExit()
         // todo: cb configMgr.save()
       }
     })
@@ -27,7 +24,7 @@ class cliVorpal {
 
   show (delimiter) {
     if (delimiter) this.delimiter = delimiter
-    if(this.callback.onShow) this.callback.onShow()
+    this.eventEmitter.emit('show')
     //todo: cb - configMgr.printMessages()
     this.vorpal
       .delimiter(this.delimiter + '$')
@@ -35,8 +32,9 @@ class cliVorpal {
   }
 
   addCommand (cmdString) {
-    const cmd_split = _.split(_.trim(cmdString), ' ')
-    this.commands[cmd_split[0]] = this.vorpal.command(cmdString)
+    const cmdSplit = cmdString.trim().split(' ')
+    this.commands[cmdSplit[0]] = this.vorpal.command(cmdString)
+    return cmdSplit[0]
   }
 
   addCommandHeader (cmdLabel) {
@@ -52,14 +50,13 @@ class cliVorpal {
           this.logger.info('command', cmdName, 'terminated with an error.')
           if (err) this.logger.info(err)
         }
-        if(this.callback.onBeforeNext) this.callback.onBeforeNext()
         // todo: cb configMgr.printMessages()
         cb()
       }, {
         cli_params: new cliParam(args, cmdName),
         error_code: ERROR_CODE,
         success_code: SUCCESS_CODE,
-        ui: clUI.newLocalUI('> ' + cmdName + ':')
+        //ui: clUI.newLocalUI('> ' + cmdName + ':')
       })
     })
   }
@@ -73,12 +70,14 @@ class cliVorpal {
     return true
   }
 
-  setCallback(event, cb) {
-    if(!Object(this.callback).hasOwnProperty(event)) {
-      this.logger.warn(`Invalid event for callback ${event}`)
+  on (eventName, cb) {
+    if(!ACCEPTED_EVENTS.includes(eventName)) {
+      this.logger.warn(`Invalid event '${eventName}'`)
       return false
     }
-    this.callback[event] = cb
+    this.eventEmitter.on(eventName, () => {
+      cb()
+    })
     return true
   }
 }
