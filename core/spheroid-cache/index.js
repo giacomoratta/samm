1ab50362859b3ef2072af536caf1d8face86c60a
Giacomo <giacomo.ratta@diymaxeda.com>
2020-01-02 12:02:56 +0100

    up

--- core/spheroid-cache/index.js
+++ core/spheroid-cache/index.js
@@ -1,15 +1,5 @@
 const SpheroidCache = require('./spheroidCache.class')
 
-/*
- * circular FIFO cache if this.max defined
- * set by label or not
- * remove by label, by id or last one
- * get by label, by id
- * first
- * last
- *
- */
-
 module.exports = {
   SpheroidCache
 }
