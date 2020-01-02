e3c0fc61dad74cb470021da078dc98b3053b02c6
Giacomo <giacomo.ratta@diymaxeda.com>
2020-01-02 12:24:13 +0100

    up

--- core/logger/__tests__/logger.test.js
+++ core/logger/__tests__/logger.test.js
@@ -4,6 +4,9 @@ const pinoLogger = require('../pino.logger')
 describe('logger(s) class and object', function() {
 
     it("should create a basic logger", function() {
+        const logger1 = basicLogger.createLogger('module1')
+        logger1.info('ax',123)
+        logger1.error('exx',123)
     })
 
     it("should create a pino logger", function() {
--- core/logger/basic.logger.js
+++ core/logger/basic.logger.js
@@ -1,38 +1,42 @@
-const pino = require('pino')({
-  level: (process.env.NODE_ENV === 'production' ? 20 : 10),
-  base: {}
-})
+const addArguments = function(extra, args) {
+  const newArgs = [ ...extra ]
+  Object.keys(args).forEach((k) => {
+    newArgs.push(args[k])
+  })
+  return newArgs
+}
 
-class basicLogger {
+class BasicLogger {
 
-  constructor(options){
-
+  constructor({ module }){
+    this.module = module
+    this.moduleLabel = `[${module}]`
   }
 
   trace(){
-
+    console.trace.apply(null,addArguments([this.moduleLabel],arguments))
   }
 
   debug(){
-
+    console.debug.apply(null,addArguments([this.moduleLabel],arguments))
   }
 
   info(){
-
+    console.info.apply(null,addArguments([this.moduleLabel],arguments))
   }
 
   warn(){
-
+    console.warn.apply(null,addArguments([this.moduleLabel],arguments))
   }
 
   error(){
-
+    console.error.apply(null,addArguments([this.moduleLabel],arguments))
   }
 
 }
 
 const createLogger = (module) => {
-  return pino.child({ module })
+  return new BasicLogger({ module })
 }
 
 module.exports = {
