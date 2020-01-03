75db9c00a8aaaea398dd1b9f909870cd18d2876b
Giacomo <giacomo.ratta@diymaxeda.com>
2020-01-04 00:08:11 +0100

    up

---
+++ app/sample/__tests__/sampleIndex.test.js
@@ -0,0 +1,7 @@
+//const { PathBasedQuery } = require('../pathBasedQuery.class')
+
+describe('SampleIndex functions', function () {
+  it('should create a sample index', function () {
+
+  })
+})
---
+++ app/sample/__tests__/sampleSet.test.js
@@ -0,0 +1,7 @@
+//const { PathBasedQuery } = require('../pathBasedQuery.class')
+
+describe('SampleSet functions', function () {
+  it('should create a sample set', function () {
+
+  })
+})
