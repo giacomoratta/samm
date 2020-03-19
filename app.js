285ce8a74488da24e137885938e668fe03b1ccc6
Giacomo <giacomo.ratta@diymaxeda.com>
2020-03-19 07:27:30 +0100

    unix-up

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ require('./globals.js');
 
 require('./app/init.js'); */
 
-require('dotenv').config()
+// require('dotenv').config()
+console.log(process.env.NODE_ENV)
+console.log(process.env.APPDATA_DIRNAME)
 process.env.ABSOLUTE_APP_PATH = process.cwd()
-require('./ui/cli-vorpal-v1/ui_start')
+// require('./ui/cli-vorpal-v1/ui_start')
